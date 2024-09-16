e23a5d790255f5385b27a90c36356036564bee7f
Ricardo Braga <ricardo.batista@opeteducation.com.br>
2024-09-16 16:32:28 -0300

    🔧 Footer

--- src/app/components/FooterMenu.tsx
+++ src/app/components/FooterMenu.tsx
@@ -1,34 +1,10 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
 import { card } from '../utils/info';
-import { CardInfo } from './Card-Info';
-import useCardStore from '../stores/CardStore';
-import iconArrowUp from '@/app/assets/icon-arrow-up.svg';
-import Image from 'next/image';
-import { Console, log } from 'console';
 import { useModalStore } from '../stores/useModalStore';
 
-
-
-
 export function FooterMenu() {
-  const setActiveId = useModalStore(state => state.setActiveId)
-
-  const setTarget = useCardStore(state => state.setTarget);
-  const setTitle = useCardStore(state => state.setTitle);
-  const activeID = useModalStore(state => state.activeId);
-  const activeTitle = useCardStore(state => state.activeTitle);
-  const [cardId, setCardId] = useState("");
-
   const { activeId, title  } = useModalStore();
-
-  
-
-  // function handler(id: number, title: string) {
-  //   setActiveId(id);
-  //   setTarget(`target${parseInt(id) + 1}`);
-  //   setTitle(title)
-  // }
 
   return (
     <div className='relative bg-sand-200 rounded-none  lg:rounded-t-[80px] w-full lg:w-[750px] pt-4 pb-4  mx-auto bg-opacity-60'>
@@ -40,8 +16,7 @@ export function FooterMenu() {
             <div className={`w-2 h-2 sm:w-4 sm:h-4 rounded-full ${activeId === index ? "bg-sand-500" : "bg-[#fff]"} `}></div>
           </li>
         ) : null
-        ))}              
-
+        ))}  
       </ul>
     </div>
 
