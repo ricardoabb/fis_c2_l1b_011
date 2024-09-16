a048659f24aa04496d04f89e1dcb0aa0130fa07d
Ricardo Braga <ricardo.batista@opeteducation.com.br>
2024-09-16 15:06:53 -0300

    🐛 BgElement

--- src/app/components/BgElements.tsx
+++ src/app/components/BgElements.tsx
@@ -1,9 +1,7 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
 
-import pointIcon from '@/app/assets/icon-point.svg';
 import Image from 'next/image';
-import useCardStore from '../stores/CardStore';
 
 type BgElementProps = {
   image: string;
@@ -15,12 +13,6 @@ type BgElementProps = {
 };
 
 export function BgElement({ image, rotation = "0deg", positionTop = "auto", positionBotton = "auto", positionLeft = "auto", positionRight = "auto" }: BgElementProps) {
-  const setActive = useCardStore(state => state.setActive)
-  const activeID = useCardStore(state => state.activeId);
-  const setTarget = useCardStore(state => state.setTarget);
-  const setTitle = useCardStore(state => state.setTitle);
-
-
 
   return (
     <>
@@ -28,16 +20,16 @@ export function BgElement({ image, rotation = "0deg", positionTop = "auto", posi
         <Image
           src={image}
           alt=''
-          layout="fill" // Faz a imagem preencher o contêiner
-          objectFit="contain" // Ajusta a imagem para cobrir o contêiner  
+          layout="fill" 
+          objectFit="contain" 
         />
       </div>
-      <div className={`absolute -z-20 top-[]  bottom-[150px]  left-[-400px] sm:left-[-100px] right-[]  rotate-[100deg]  w-[600px] h-[600px] select-none`}>
+      <div className={`absolute -z-20 top-[-80px]  bottom-[150px]  left-[-400px] sm:left-[-100px] right-[]  rotate-[100deg]  w-[600px] h-[600px] select-none`}>
         <Image
           src={image}
           alt=''
-          layout="fill" // Faz a imagem preencher o contêiner
-          objectFit="contain" // Ajusta a imagem para cobrir o contêiner  
+          layout="fill"
+          objectFit="contain"
         />
       </div>
     </>
